1dfbc69f92d846b951e46b334dd8e3cc29d29000
Josh Juran <jjuran@metamage.com>
2012-10-07 20:58:35 -0700

    Add the null Android app

---
+++ src/Noisegate.java
@@ -0,0 +1,19 @@
+package com.metamage.noisegate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public final class Noisegate extends Activity
+{
+	
+	@Override
+	protected void onCreate( Bundle savedInstanceState )
+	{
+		super.onCreate( savedInstanceState );
+		
+		setContentView( R.layout.main );
+	}
+	
+}
+
