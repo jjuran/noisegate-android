644c286235b40219f16d3993c49417f1aad0970b
Josh Juran <jjuran@metamage.com>
2012-10-22 05:09:44 -0700

    Omit window title bar

--- src/Noisegate.java
+++ src/Noisegate.java
@@ -2,6 +2,7 @@ package com.metamage.noisegate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
 
 public final class Noisegate extends Activity
@@ -11,6 +12,8 @@ public final class Noisegate extends Activity
 	protected void onCreate( Bundle savedInstanceState )
 	{
 		super.onCreate( savedInstanceState );
+		
+		requestWindowFeature( Window.FEATURE_NO_TITLE );
 		
 		setContentView( R.layout.main );
 	}
