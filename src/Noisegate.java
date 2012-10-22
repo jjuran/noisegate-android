b8de6223a515d8a9de0831079c745e5e7a27bcc7
Josh Juran <jjuran@metamage.com>
2012-10-22 03:54:35 -0700

    Add key handler stubs

--- src/Noisegate.java
+++ src/Noisegate.java
@@ -2,11 +2,24 @@ package com.metamage.noisegate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 
 public final class Noisegate extends Activity
 {
+	
+	public void onNumericKey( View v )
+	{
+	}
+	
+	public void onClearKey( View v )
+	{
+	}
+	
+	public void onEnterKey( View v )
+	{
+	}
 	
 	@Override
 	protected void onCreate( Bundle savedInstanceState )
