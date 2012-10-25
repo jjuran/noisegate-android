b710798f270521a87dd2b2be85d070cfa5fddd72
Josh Juran <jjuran@metamage.com>
2012-10-25 06:37:58 -0700

    Buffer the user-entered code

--- src/Noisegate.java
+++ src/Noisegate.java
@@ -4,21 +4,31 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 
 public final class Noisegate extends Activity
 {
 	
+	private String code = "";
+	
 	public void onNumericKey( View v )
 	{
+		Button key = (Button) v;
+		
+		code += key.getText();
 	}
 	
 	public void onClearKey( View v )
 	{
+		code = "";
 	}
 	
 	public void onEnterKey( View v )
 	{
+		if ( code.length() != 0 )
+		{
+		}
 	}
 	
 	@Override
