eeddd5c6777c7e960c2bc8b24fabd53b6190a4f0
Josh Juran <jjuran@metamage.com>
2012-12-11 00:14:19 -0800

    Add Data.fakeKeyIds[]
    Store the ids of the fake numeric keys in an array, indexed by the key's
    name.  This is an easy way to get from the live key to the fake one.

--- src/Data.java
+++ src/Data.java
@@ -7,5 +7,19 @@ final public class Data
 	static public int normalColor;
 	static public int pressedColor;
 	
+	static public int[] fakeKeyIds =
+	{
+		R.id._0,
+		R.id._1,
+		R.id._2,
+		R.id._3,
+		R.id._4,
+		R.id._5,
+		R.id._6,
+		R.id._7,
+		R.id._8,
+		R.id._9
+	};
+	
 }
 
